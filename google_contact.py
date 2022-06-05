efd4c1f4e7597c88b76b1627d81f5dfe35ab2944
bhagirath-radadiya <59503020+bhagirath-radadiya@users.noreply.github.com>
2022-06-05 12:17:03 +0530

    Create google contact

---
+++ google_contact.py
@@ -0,0 +1,33 @@
+import time
+
+# selenium
+from selenium import webdriver
+from selenium_stealth import stealth
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_argument(
+    "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36")
+options.add_argument("--user-data-dir=/home/bhagirath/.config/google-chrome/")
+options.add_argument("--profile-directory=Default")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome(options=options, executable_path="< chrome driver path >")
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Ubuntu 20.04.3 LTS",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+
+driver.get("https://contacts.google.com/")
+time.sleep(30)
+driver.get("https://contacts.google.com/new")
+driver.find_element(By.XPATH, '//input[@aria-label="First name"]').send_keys("< contact name >")
+driver.find_element(By.XPATH, '//input[@aria-label="Phone"]').send_keys("<contact number with country code>")
+driver.find_element(By.XPATH, '//button[@aria-label="Save"]').click()
